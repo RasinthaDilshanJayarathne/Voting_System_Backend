0db8fa338d127f419064678dc822cedc9e2c3f52
rasintha_j <rasintha_j@epiclanka.net>
2023-06-20 19:56:08 +0530

    Create all CRUD Operations....

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ headers = {
 
 app = Flask(__name__)
 
+# DB CONNECTION
+
 
 def db_connection():
     conn = None
@@ -26,6 +28,7 @@ def db_connection():
     return conn
 
 
+# CANDIDATE
 @app.route('/candidate', methods=['GET', 'POST'])
 def candidates():
     conn = db_connection()
@@ -69,10 +72,8 @@ def single_candidate(nic):
             print(f"Error creating candidate: {e}")
             return "Failed to create candidate", 500
 
-
     if request.method == 'PUT':
         candidate_data = request.get_json()
-
         try:
             sql = "UPDATE candidate SET first_name = %(first_name)s, last_name = %(last_name)s, nic = %(nic)s, email = %(email)s, gender = %(gender)s, age = %(age)s, election_party = %(election_party)s, vote_no = %(vote_no)s, password = %(password)s, conform_password = %(conform_password)s WHERE nic = %(nic)s"
             candidate_data['nic'] = nic
@@ -92,6 +93,138 @@ def single_candidate(nic):
         except mysql.connector.Error as e:
             print(f"Error creating candidate: {e}")
             return "Failed to create candidate", 500
+
+# VOTER
+@app.route('/voter', methods=['GET', 'POST'])
+def voters():
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM voter")
+        voters = cursor.fetchall()
+        if voters:
+            return jsonify(voters), 200
+        else:
+            return "No voter found", 404
+
+    if request.method == 'POST':
+        voter_data = request.get_json()
+    try:
+        sql = "INSERT INTO voter (first_name, last_name, nic, email, gender, age, password, conform_password) VALUES (%(first_name)s, %(last_name)s, %(nic)s, %(email)s, %(gender)s, %(age)s, %(password)s, %(conform_password)s)"
+        cursor.execute(sql, voter_data)
+        conn.commit()
+        return "Voter created successfully", 201
+    except mysql.connector.Error as e:
+        print(f"Error creating voter: {e}")
+        return "Failed to create voter", 500
+
+
+@app.route('/voter/<string:nic>', methods=['GET', 'PUT', 'DELETE'])
+def single_voter(nic):
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    if request.method == 'GET':
+        try:
+            cursor.execute(
+                "SELECT * FROM voter WHERE nic = %(nic)s", {'nic': nic})
+            voter = cursor.fetchone()
+            if voter:
+                return jsonify(voter), 200
+            else:
+                return "Voter not found", 404
+        except mysql.connector.Error as e:
+            print(f"Error creating voter: {e}")
+            return "Failed to create voter", 500
+
+    if request.method == 'PUT':
+        voter_data = request.get_json()
+        try:
+            sql = "UPDATE voter SET first_name = %(first_name)s, last_name = %(last_name)s, nic = %(nic)s, email = %(email)s, gender = %(gender)s, age = %(age)s, password = %(password)s, conform_password = %(conform_password)s WHERE nic = %(nic)s"
+            voter_data['nic'] = nic
+            cursor.execute(sql, voter_data)
+            conn.commit()
+            return "Voter updated successfully", 200
+        except mysql.connector.Error as e:
+            print(f"Error creating voter: {e}")
+            return "Failed to create voter", 500
+
+    if request.method == 'DELETE':
+        try:
+            sql = "DELETE FROM voter WHERE nic = %(nic)s"
+            cursor.execute(sql, {'nic': nic})
+            conn.commit()
+            return "Voter deleted successfully", 200
+        except mysql.connector.Error as e:
+            print(f"Error creating voter: {e}")
+            return "Failed to create voter", 500
+
+#QUOTA
+@app.route('/quota', methods=['GET', 'POST'])
+def quotas():
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM quota")
+        quotas = cursor.fetchall()
+        if quotas:
+            return jsonify(quotas), 200
+        else:
+            return "No quota found", 404
+
+    if request.method == 'POST':
+        quota_data = request.get_json()
+    try:
+        sql = "INSERT INTO quota (quota_name,quota_reg_no,no_of_people,registerd_voters) VALUES (%(quota_name)s, %(quota_reg_no)s, %(no_of_people)s, %(registerd_voters)s)"
+        cursor.execute(sql, quota_data)
+        conn.commit()
+        return "Quota created successfully", 201
+    except mysql.connector.Error as e:
+        print(f"Error creating quota: {e}")
+        return "Failed to create quota", 500
+
+
+@app.route('/quota/<string:quota_reg_no>', methods=['GET', 'PUT', 'DELETE'])
+def single_quota(quota_reg_no):
+    conn = db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    if request.method == 'GET':
+        try:
+            cursor.execute(
+                "SELECT * FROM quota WHERE quota_reg_no = %(quota_reg_no)s", {'quota_reg_no': quota_reg_no})
+            quota = cursor.fetchone()
+            if quota:
+                return jsonify(quota), 200
+            else:
+                return "Quota not found", 404
+        except mysql.connector.Error as e:
+            print(f"Error creating quota: {e}")
+            return "Failed to create quota", 500
+
+    if request.method == 'PUT':
+        quota_data = request.get_json()
+        try:
+            sql = "UPDATE quota SET quota_name = %(quota_name)s, no_of_people = %(no_of_people)s, registerd_voters = %(registerd_voters)s WHERE quota_reg_no = %(quota_reg_no)s"
+            quota_data['quota_reg_no'] = quota_reg_no
+            cursor.execute(sql, quota_data)
+            conn.commit()
+            return "Voter updated successfully", 200
+        except mysql.connector.Error as e:
+            print(f"Error creating quota: {e}")
+            return "Failed to create quota", 500
+
+    if request.method == 'DELETE':
+        try:
+            sql = "DELETE FROM quota WHERE quota_reg_no = %(quota_reg_no)s"
+            cursor.execute(sql, {'quota_reg_no': quota_reg_no})
+            conn.commit()
+            return "Quota deleted successfully", 200
+        except mysql.connector.Error as e:
+            print(f"Error creating quota: {e}")
+            return "Failed to create quota", 500
 
 
 if __name__ == "__main__":
